70d8d57e52daaa0d8879600cd474b9b7f522f4f9
JeongkiSon <sonkkii27521@gmail.com>
2022-08-21 01:26:31 +0900

    check

--- algorithms/binary search/prob2.py
+++ algorithms/binary search/prob2.py
@@ -17,5 +17,10 @@ else:
     print(b-a)
            
 
-
-
+'''
+값이 [left_value , right_value] 인 데이터의 개수를 반환하는 함수
+def count_by_range(arr, left_value, right_value):
+    right_index = bisect_right(arr, right_value)
+    left_index = bisect_left(arr, left_value)
+    return right_index - left_index
+'''
